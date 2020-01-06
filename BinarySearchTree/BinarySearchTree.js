6949bd4ae8dd834543ef2474ff1305cfa8611462
pg035053 <prachi.gotkhindikar@cerner.com>
2020-01-06 06:43:05 -0600

    Add Tree Traversals

--- BinarySearchTree/BinarySearchTree.js
+++ BinarySearchTree/BinarySearchTree.js
@@ -56,6 +56,64 @@ class BinarySearchTree {
         if(!found) return undefined;
         return current;
     }
+
+// Breadth First Search
+    BFS() {
+        var data = [],
+        queue = [],
+        current = this.root;
+
+        queue.push(current);
+        while(queue.length){
+            var node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right)
+        }
+        return data;
+    }
+
+// Depth first search - PreOrder
+ DFSPreOrder() {
+     var data = [], current = this.root;
+     function traverse(node){
+         data.push(node.value);
+         if(node.left) traverse(node.left);
+         if(node.right) traverse(node.right);
+     }
+     traverse(current);
+     return data;
+ }
+
+// Depth first search - InOrder
+ DFSInOrder() {
+     var data = [],
+     current = this.root;
+
+     function traverse(node){
+         if(node.left) traverse(node.left);
+         data.push(node.value);
+         if(node.right) traverse(node.right);
+     }
+
+     traverse(current);
+     return data;
+ } 
+
+// Depth first search - PostOrder
+ DFSPostOrder() {
+     var data= [],
+     current = this.root;
+
+     function traverse(node){
+         if(node.left) traverse(node.left);
+         if(node.right) traverse(node.right);
+         data.push(node.value);
+     }
+
+     traverse(current);
+     return data;
+ }     
 }
 
 var tree = new BinarySearchTree();
