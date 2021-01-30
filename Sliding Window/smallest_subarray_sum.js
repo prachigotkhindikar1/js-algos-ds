e0cb49375fb334d3a85a71b4b3902fa3b21e2d1b
Prachi Gotkhindikar <prachigotkhindikar1@gmail.com>
2021-01-29 20:17:07 -0600

    Update smallest_subarray_sum.js

--- Sliding Window/smallest_subarray_sum.js
+++ Sliding Window/smallest_subarray_sum.js
@@ -1,6 +1,11 @@
 // Given an array of positive numbers and a positive number ‘S,’ find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’
 
 // Brute force: O(n2)
+// Algorithm: 
+// 1. Start the outerloop with the first number and run it till length of array.
+// 2. Inner loop: Starting with i+1, add the nos until sum >= target sum.
+// 3. If we have the condition satisfied. Calculate the length of array and set the minLength and break.
+
 const smallest_subarray_with_given_sum = function(s, arr) {
   let minLength = Infinity;
   for(let i=0; i<arr.length; i++){
@@ -18,4 +23,18 @@ const smallest_subarray_with_given_sum = function(s, arr) {
 };
 
 // Sliding window - Dynamic window
-
+const smallest_subarray_with_given_sum = function(s, arr) {
+  let windowStart = 0, minLength = Infinity, windowSum=0;
+  for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+     windowSum += arr[windowEnd];
+    
+    while(windowSum >=s){
+      minLength = Math.min(minLength, windowEnd-windowStart+1);
+      windowSum -= arr[windowStart];
+      windowStart += 1;
+  }
+    
+    if(minLength === Infinity) return 0;
+    
+    return minLength;
+}
