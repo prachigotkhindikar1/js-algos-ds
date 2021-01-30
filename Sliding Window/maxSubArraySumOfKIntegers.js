118686f005a1713ffa13f581ab959f0dfb219726
Prachi Gotkhindikar <prachigotkhindikar1@gmail.com>
2021-01-29 18:54:46 -0600

    Update maxSubArraySumOfKIntegers.js

--- Sliding Window/maxSubArraySumOfKIntegers.js
+++ Sliding Window/maxSubArraySumOfKIntegers.js
@@ -39,4 +39,22 @@
     
         }
         maxSubArraySum([-1,-2,-2], 2)
+
+
+
+// Sliding window = My soln
+const max_sub_array_of_size_k = function(k, arr) {
+    let maxSum = 0,  windowSum = 0, windowStart = 0;
+    for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+      windowSum+=arr[windowEnd];
+
+      if(windowEnd >= windowStart+k-1){
+        maxSum = Math.max(maxSum, windowSum);
+        windowSum -= arr[windowStart];
+        windowStart++;
+      }
+    }
+    return maxSum;
+};
+
     
